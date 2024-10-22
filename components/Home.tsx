fd52ec36d339e7fd44d25f0776f0611ad349bdb1
milesmoralespro <145787286+milesmoralespro@users.noreply.github.com>
2024-10-22 16:47:57 +0530

    Update Home.tsx

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useState } from 'react'
-import Link from "next/link"
 import { motion, AnimatePresence } from 'framer-motion'
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
